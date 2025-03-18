fcf1a8f479d24281adc0a4ac981827a937d88e2c
zy-zy-oss <1484529904@qq.com>
2025-03-18 16:40:17 +0800

    deploy

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -5,17 +5,13 @@ import PieChart from './PieChart';
 import BarChart from './BarChart';
 import DataSummary from './DataSummary';
 import { salesData, userDistribution, productCategories } from '../data/mockData';
-
 const { Content } = Layout;
-
-
 const DashboardLayout = () => {
   const [timeRange, setTimeRange] = useState('monthly');
 
   const handleTimeRangeChange = (e) => {
     setTimeRange(e.target.value);
   };
-
   return (
     <Layout>
       <Content style={{ padding: '20px', maxWidth: '1400px', margin: '0 auto' }}>
@@ -24,26 +20,19 @@ const DashboardLayout = () => {
             <Radio.Button value="monthly">月度</Radio.Button>
             <Radio.Button value="quarterly">季度</Radio.Button>
           </Radio.Group>
-        </div>
-        
+        </div>  
         <Row gutter={[16, 16]}>
-          {/* 数据汇总卡片 */}
           <Col span={24}>
             <DataSummary data={salesData[timeRange]} />
           </Col>
-
-          {/* 销售趋势图 */}
           <Col span={24}>
             <Card 
               title="销售趋势" 
-              bodyStyle={{ height: 400 }} // 统一高度，或通过媒体查询自定义
+              bodyStyle={{ height: 400 }} 
             >
               <LineChart data={salesData[timeRange]} />
             </Card>
           </Col>
-
-          {/* 用户分布图 & 产品分析 */}
-          {/* 使用 xs/md 等断点控制响应式布局 */}
           <Col xs={24} md={12}>
             <Card 
               title="用户分布" 
